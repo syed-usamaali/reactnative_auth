dac14cf5778a71380ccd5312be459fae0794a946
usama-aoitechs <61180500+usama-aoitechs@users.noreply.github.com>
2020-02-21 16:37:07 +0500

    performed login and logout

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,45 @@ import firebase from 'firebase';
 import {Card, CardSection, Button, Input, Spinner} from './common';
 
 class LoginForm extends Component {
-  state = {email: '', password: '', error: ''};
+  state = {email: '', password: '', error: '', loading: false};
 
   onButtonPress() {
     const {email, password} = this.state;
 
-    this.setState({error: ''});
+    this.setState({error: '', loading: true});
 
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase
           .auth()
           .createUserWithEmailAndPassword(email, password)
-          .catch(() => this.setState({error: 'Authentication Failed'}));
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFailed.bind(this));
       });
+  }
+
+  onLoginFailed() {
+    this.setState({error: 'Authentication Failed', loading: false});
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '',
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return <Button onPress={this.onButtonPress.bind(this)}>Login</Button>;
   }
 
   render() {
@@ -46,9 +69,7 @@ class LoginForm extends Component {
 
         <Text style={styles.errorStyle}>{this.state.error}</Text>
 
-        <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
